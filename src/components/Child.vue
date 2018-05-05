f0d4a3c21af00b52b58010bed4d09e1f73605605
gyt95 <gyt95@163.com>
2018-05-05 23:27:54 +0800

    this.$set

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -5,39 +5,72 @@
       <br><br>
       <button @click="sum = !sum">...</button>
       {{sum}}
+      <br><br>
+
+
+      
+      <p>测试引用类型属性修改子组件视图更新情况</p>
+      <ul>
+          <li v-for="item in arrData" :key="item.id">
+              <button @click="handleMod(item)">click</button>
+              {{item.name}}&nbsp;&nbsp;&nbsp;&nbsp;{{item.status}}
+              
+              <p>{{item.city}}a</p>
+
+              <span v-for="item2 in item.lis" :key="item2.id">
+                  {{item2.isCorrect}}
+              </span>
+          </li>
+      </ul>
+      <br><br>
+      <ul>
+          <li v-for="(val,index) in objData" :key="index">
+              {{val}}
+          </li>
+      </ul>
   </div>
 </template>
 <script>
 export default {
-  props:{
-      isFlag:{
-          type:Boolean
-      }
+  props: {
+    isFlag: {
+      type: Boolean
+    },
+    arrData: {
+      type: Array
+    },
+    objData: {
+        type: Object
+    }
   },
-  data(){
-      return{
-          arr:[
-              {id:1,name:'jack'},
-              {id:2,name:'ben'},
-              {id:3,name:'ken'}
-          ],
-          sum:false
-      }
+  data() {
+    return {
+      arr: [
+        { id: 1, name: "jack" },
+        { id: 2, name: "ben" },
+        { id: 3, name: "ken" }
+      ],
+      sum: false
+    };
   },
-  watch:{   // 监听某个属性，变化则被调用
-      'isFlag':function(){
-          alert(this.isFlag)
-          this.send()
-      },
-      'sum':function(){
-          console.log(this.isFlag)
-      }
+  watch: {
+    // 监听某个属性，变化则被调用
+    isFlag: function() {
+      alert(this.isFlag);
+      this.send();
+    },
+    sum: function() {
+      console.log(this.isFlag);
+    }
   },
-  methods:{
-      send(){
-          console.log('全选/全清除')
-          console.log(this.arr)
-      }
+  methods: {
+    send() {
+      console.log("全选/全清除");
+      console.log(this.arr);
+    },
+    handleMod(item){
+        this.$emit('onClick',item)
+    }
   }
-}
+};
 </script>
--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -3,35 +3,114 @@
       Parent
       <button @click="change()">全选</button>
       <button @click="clear()">全清除</button>
+      <button @click="modify()">修改name</button>
       <Child 
         :isFlag="flag"
+        :arrData="arr"
+        :objData="obj"
+        @onClick="modify2"
       />
   </div>
 </template>
 <script>
-import Child from './Child.vue'
+import Child from "./Child.vue";
 export default {
-  components:{
-      Child
+  components: {
+    Child
   },
-  data(){
-      return{
-          flag: false
+  data() {
+    return {
+      flag: false,
+      arr: [
+        {
+          id: 1,
+          name: 'aaa',
+          lis:[
+              {
+                id:1,
+                birth:1995,
+              },
+              {
+                id:2,
+                birth:1990,
+              }
+          ]
+        },
+        {
+          id: 2,
+          name: 'bbb',
+          lis:[
+              {
+                id:1,
+                birth:2001,
+              },
+              {
+                id:2,
+                birth:2005,
+              }
+          ]
+        }
+      ],
+      obj:{
+          id:1,
+          name:'ken',
+          age:18
       }
+    };
   },
-  mounted(){
-      this.run()
+  mounted() {
+    this.run();
   },
-  methods:{
-      change(){
-          this.flag = true;
+  methods: {
+      modify2(item){
+          item.city = '上海'
       },
-      clear(){
-          this.flag = false;
-      },
-      run(){
-          console.log('每次更新我都被调用?')
-      }
+    modify() {
+        this.arr.map(v => {
+            v.lis.map(v2 => {
+                if(v2.id === 2){
+                    v2 = Object.assign(v2, {
+                        isCorrect:true
+                    })
+                }else{
+                    v2 = Object.assign(v2, {
+                        isCorrect:false
+                    })
+                }
+            })
+        })
+    // vue若直接对已有数组的每个对象增加新属性，子组件的视图是不能及时更新的
+      this.arr[0].name = 'kkk';
+      this.arr.map((v,i) => {
+        // v = Object.assign({}, v, {status:'ok'})不行   
+
+        //   v = Object.assign(v, {
+        //       status:'ok'
+        //   })
+        if(v.id===1) this.$set(v, 'status', 'good!')
+      })
+
+    // 不可行例子
+    this.arr.map(v => {
+        return v.city = 'gz'
+    })
+
+    // 而vue是允许当父组件直接对一个对象动态增加新属性时，子组件能及时更新视图的
+    //   this.obj.staff = 'police'
+    //   this.obj.name = 'ggggg'
+
+
+    /**  结论：为啥我之前项目中又不行呢？？？ */
+    },
+    change() {
+      this.flag = true;
+    },
+    clear() {
+      this.flag = false;
+    },
+    run() {
+      console.log("每次更新我都被调用?");
+    }
   }
-}
+};
 </script>
